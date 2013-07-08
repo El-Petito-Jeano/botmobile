dcd36556ed88c6ef06b03eccb50c735067c4fbb8
El-Petito-Jeano <larde.jean@gmail.com>
2013-07-08 13:39:24 +0200

    Update access.rb

--- access.rb
+++ access.rb
@@ -2,21 +2,21 @@ require 'rubygems'
 require 'nokogiri'
 require 'active_record'
 
-# all the great different way to access the db, to
-# get information in it or to put new infos
+# all the great different ways to access the db, to
+# get information in it or to put new
 class Access
 
         def add_debt_to (person, driver, amount)
                 # if the person has never went with the driver, just create the new entry
-                # add to table :debts the amount for the person to the driver
+                # add to table :debts rpz the amount for the person to the driver
         end # add_debt_to (person, driver, amount)
 
         def has_paid (driver, person, amount)
                 # debt - amount
-        end # has_paid
+        end # has_paid (driver, perso, amount)
 
         def reset_debt_of (person, driver)
                 # delet the debt of the person to the driver
-        end # reset_deb_of (perso)
+        end # reset_deb_of (person, driver)
 
 end # class
